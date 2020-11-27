c765f5e6e3d3ab4d9755dae5aefef58b6d5de518
Johannes Lauter <johannes.lauter@sparwelt.de>
2020-11-27 18:21:34 +0100

    🔥 removing temperoray storybook stuff

--- .nuxt-storybook/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as DsButton } from '../..\\components\\ds\\button.vue'
-export { default as DsCard } from '../..\\components\\ds\\card.vue'
-
-export const LazyDsButton = import('../..\\components\\ds\\button.vue' /* webpackChunkName: "components_ds/button" */).then(c => c.default || c)
-export const LazyDsCard = import('../..\\components\\ds\\card.vue' /* webpackChunkName: "components_ds/card" */).then(c => c.default || c)
